3507f4d925bdf0107a2230263b2ca953a58d9fb3
lixiaofeng <1130034920@qq.com>
2021-06-24 23:36:42 +0800

    6-11 nodejs 连接redis-封装工具函数

--- blog1/src/conf/db.js
+++ blog1/src/conf/db.js
@@ -2,6 +2,7 @@ const env = process.env.NODE_ENV // 环境变量
 
 // 配置
 let MYSQL_CONF
+let REDIS_CONF
 
 if (env === 'dev') {
     // mysql
@@ -10,6 +11,11 @@ if (env === 'dev') {
         user: 'root',
         password: '123456',
         database: 'myblog'
+    }
+    // redis
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
     }
 }
 
@@ -21,8 +27,14 @@ if (env === 'production') {
         password: '123456',
         database: 'myblog'
     }
+    // redis
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
+    }
 }
 
 module.exports = {
-    MYSQL_CONF
+    MYSQL_CONF,
+    REDIS_CONF
 }
