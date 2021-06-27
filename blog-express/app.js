bc6dbbc9ad8909f5fa11e2c1133481072795c98c
lixiaofeng <1130034920@qq.com>
2021-06-27 18:38:44 +0800

    9-3 介绍express的入口代码

--- blog-express/app.js
+++ blog-express/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express(); // 一个http请求实例
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,8 +16,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser()); // 解析cookie
+app.use(express.static(path.join(__dirname, 'public'))); // 静态资源路径处理
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
